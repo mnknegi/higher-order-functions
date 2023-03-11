e890fc51ae05852d650aeecc71e6bb89dcb3ab7b
imayanknegi29 <imayanknegi29@gmail.com>
2023-03-11 23:08:41 +0530

    compactMap and flatMap

--- HigherOrderFunctions.playground/Contents.swift
+++ HigherOrderFunctions.playground/Contents.swift
@@ -1,13 +1,9 @@
 import Foundation
 
-/*
-let sorted = Sorted()
-sorted.printDescendingOrder()
-*/
+// MARK: - Map
 
-//------------------------<Map>-----------------------//
 /*
- Without using higher order function
+ Without using Map function
 
 let coins = [1, 2, 5, 10, 20, 50, 100, 200, 500, 2000]
 
@@ -27,7 +23,7 @@ let coins = [1, 2, 5, 10, 20, 50, 100, 200, 500, 2000]
 
 let indianRupee = coins.map({ "\u{20B9} \($0)" })
 
-debugPrint("Indian Rupee coins: ", indianRupee)
+debugPrint("Indian Rupee coins: \(indianRupee)")
 
 /// Dictionary
 let info = ["name": "foo",
@@ -39,6 +35,52 @@ let info = ["name": "foo",
 //let updatedInfo = info.map { ($0.key.uppercased(), $0.value.capitalized) }
 let updatedInfo = info.map { ($0.uppercased(), $1.capitalized) }
 
-print("Employee info: ", updatedInfo)
+debugPrint("Employee info: \(updatedInfo)")
 
-//------------------------</Map>-----------------------//
+
+// MARK: - CompactMap
+
+let numbers = [2,6,10, nil, 49, 28, nil, 15, nil, 60]
+
+let nonNilNumbers = numbers.compactMap { $0 != nil ? $0! * 2 : nil }
+
+debugPrint("Collection with non nil elements: \(nonNilNumbers)")
+
+// MARK: - FlatMap
+ 
+let sectionAStudents = ["Mayank", "Mahima", "Ashish"]
+let sectionBStudents = ["Ved", "Vaasu"]
+let sectionCStudents = ["Mahendra", "Laxmi"]
+
+let class10Students = [sectionAStudents, sectionBStudents, sectionCStudents]
+
+/*
+ Without using Map function
+
+var allStudents = [String]()
+for students in class10Students {
+    allStudents.append(contentsOf: students)
+}
+*/
+
+var allStudents = class10Students.flatMap { $0 }
+
+debugPrint("All class 10th students: ", allStudents)
+
+
+// flatMap with nil values in inner collection
+let valuesWithNil = [[2, nil, 5], [4, 3, nil], [nil, nil, 1]]
+
+let flatMapResult = valuesWithNil.flatMap { $0 }
+
+debugPrint("non nil values: ", flatMapResult)
+
+// Dictionary
+
+let dict: [[String: Any]] = [["name": "Mayank", "age": 30],
+            ["job": "iOS Developer", "title": "Tech Lead"],
+            ["location": "Kotdwara", "state": "Uttarakhand", "zip": "246149"]]
+
+let updatedDict = dict.flatMap { $0 }
+
+debugPrint("Employee Info: ", updatedDict)
